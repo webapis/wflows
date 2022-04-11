387890044c27566b5056f1876c2728e1e6dc3884
webapis <60083568+webapis@users.noreply.github.com>
2022-04-11 08:28:01 +0500

    c

--- firebase/firebase-rest.js
+++ firebase/firebase-rest.js
@@ -209,7 +209,7 @@ async function renewIdToken({ api_key, refresh_token, localId }) {
 
     const { id_token, refresh_token: newRefreshToken } = await response.json()
     //update firebase
-    const fetchUrl = `${window.projectUrl}/.json?auth=${id_token}`
+    const fetchUrl = `${process.env.projectUrl}/.json?auth=${id_token}`
     const responses = await fetch(fetchUrl, { method: 'PATCH', body: JSON.stringify({ [`oauth/users/${localId}/firebase/idToken`]: id_token, [`oauth/users/${localId}/firebase/refreshToken`]: newRefreshToken,[`oauth/users/${localId}/firebase/date`]: new Date()}) })
 
     //update env
